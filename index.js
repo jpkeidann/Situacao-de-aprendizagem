0fcd1486665e9ac21e11b4c4f7c54b1245d1ae6c
jpkeidann <joao_keidann@estudante.sesisenai.org.br>
2025-11-19 10:16:19 -0300

    coisa de eletroeletronica

---
+++ index.js
@@ -0,0 +1,25 @@
+function redirecionar(pagina) {
+    switch (pagina){
+        case 0:
+            window.location.href = "index.html";
+            break;
+        case 1:
+            window.location.href = "fundsEletro.html";
+            break;
+        case 2:
+            window.location.href = "logComp.html";
+            break;
+        case 3:
+            window.location.href = "FundsBancDad.html";
+            break;
+        case 4:
+            window.location.href = "FundsRedes.html";
+            break;
+        case 5:
+            window.location.href = "LogProg.html";
+            break;
+        case 6:
+            window.location.href = "FundsTecInf.html";
+            break;
+    }
+}
